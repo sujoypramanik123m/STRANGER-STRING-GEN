ee62752af98adaa391c6b9c62181a2b7f8c246b8
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-04 13:52:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","27383453"))
 API_HASH = getenv("API_HASH","4c246fb0c649477cc2e79b6a178ddfaa")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","6762113050"))
+OWNER_ID = int(getenv("OWNER_ID","8181241262"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN","SHIVANSH474")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("MUST_JOIN","ProToppers")
